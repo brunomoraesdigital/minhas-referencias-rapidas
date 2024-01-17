ae316546f916e14d7dd797e2f41780fd250cb70d
Bruno Moraes <brunomoraes.dev@gmail.com>
2024-01-16 22:30:44 -0300

    160124.2230

--- js/engine.js
+++ js/engine.js
@@ -2,3 +2,18 @@ const anoAtualElement = document.getElementById("ano-atual");
 const anoAtual = new Date().getFullYear();
 anoAtualElement.textContent = `Copyright ©
 ${anoAtual} Bruno Moraes`;   
+
+/* */
+
+document.addEventListener("DOMContentLoaded", function() {
+    var navIcon = document.getElementById("nav-icon");
+    var navigation = document.getElementById("navigation");
+    var navClose = document.getElementById("nav-close");
+
+    navIcon.addEventListener("click", function() {
+        navigation .classList.toggle("open");
+    });
+    navClose.addEventListener("click", function() {
+        navigation .classList.toggle("open");
+    });
+  });
