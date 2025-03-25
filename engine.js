f7d07b1e85a3d819a76e1c8ad1ef71be6297689a
Bruno Moraes <89628890+brunomoraesdigital@users.noreply.github.com>
2025-03-25 20:26:24 +0000

    250325.1726

--- engine.js
+++ engine.js
@@ -1,293 +1,229 @@
-/* ******** */
-/* contador */
-/* ******** */
+/* **********
+ * LOADING  *
+ ********** */
 
-// Acessar os elementos HTML pelos seus IDs para poder manipulá-los no JavaScript
+let canvasLoading = document.getElementById("canvasLoading");
+let ctx = canvasLoading.getContext("2d");
 
-// Seleciona o elemento que tem o ID 'contador' da página HTML
-// A variável contadorElemento guarda o elemento com o ID 'contador'
-const contadorElemento = document.getElementById('contador');
+let tamanhoPixel = 3;
 
-// Seleciona o botão que tem o ID 'incrementa' da página HTML
-// A variável 'incrementaBotao' guarda o botão que tem o ID 'incrementa'
-const incrementaBotao = document.getElementById('incrementa');
+let magoParado = [
+  [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0],
+  [0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0]
+];
 
-// Seleciona o botão que tem o ID 'decrementa' da página HTML
-// A variável 'decrementaBotao' guarda o botão que tem o ID 'decrementa'
-const decrementaBotao = document.getElementById('decrementa');
+let magoAndando1 = [
+  [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0],
+  [1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
 
-// Define uma variável 'numero' para armazenar o valor do contador
-// Inicialmente, começamos com o valor de 'numero' igual a 0
-let numero = 0;
+];
 
-// Define a função 'manipularNumero' que será executada quando o botão de incremento for clicado
-function manipularNumero() {
-    numero++; // Aumenta o valor de 'numero' em 1 (equivalente a numero = numero + 1)
+let magoAndando2 = [
+  [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+  [0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0]
+]
 
-    // Atualiza o conteúdo do elemento 'contadorElemento' para mostrar o valor de 'numero'
-    contadorElemento.textContent = numero; // O número na tela vai mudar para o novo valor de 'numero'
-}
+// Definindo o objeto mago
+let mago = {
+  parado: {
+    nome: "parado",
+    matriz: magoParado
+  },
+  andando1: {
+    nome: "andando1",
+    matriz: magoAndando1
+  },
+  andando2: {
+    nome: "andando2",
+    matriz: magoAndando2
+  },
 
-// Função chamada 'diminuir' que será executada quando o botão de decremento for clicado
-function diminuir() {
-    numero--; // Diminui o valor de 'numero' em 1 (equivalente a numero = numero - 1)
-
-    // Atualiza o conteúdo do elemento 'contadorElemento' para mostrar o valor de 'numero'
-    contadorElemento.textContent = numero; // O número na tela vai mudar para o novo valor de 'numero'
-}
-
-// Adiciona um evento de "clique" ao botão de incremento
-// Quando o botão de incremento for clicado, a função 'manipularNumero' será chamada
-incrementaBotao.addEventListener('click', manipularNumero);
-
-// Adiciona um evento de "clique" ao botão de decremento
-// Quando o botão de decremento for clicado, a função 'diminuir' será chamada
-decrementaBotao.addEventListener('click', diminuir);
-
-/* ************** */
-/* Mudança de cor */
-/* ************** */
-
-// Seleciona o botão que tem o ID 'mudarCorFundo' da página HTML
-// A variável mudarCorFundoBotao guarda o elemento com o ID 'mudarCorFundo'
-const mudarCorFundoBotao = document.getElementById('mudarCorFundo');
-
-// Define a função mudarACor que será chamada quando o botão for clicado
-function mudarACor() {
-    // Define uma variável 'numAleatorio' e inicializa com um número gerado aleátoriamente entre 0 e 360, para usar como o "matiz" (hue) na cor HSL
-    // Math.random() gera um número entre 0 (inclusive) e 1 (exclusivo)
-    // Multiplicamos esse valor por 360 para ter um número aleatório entre 0 e 360
-    // Math.floor arredonda o valor para baixo, então o número vai ser um inteiro entre 0 e 359
-    let numAleatorio = Math.floor(Math.random() * 360);
-
-    // Define uma variável 'corAleatoria' e armazena com uma cor aleatória no formato HSL
-    // A string 'hsl(${numAleatorio}, 70%, 80%)' vai usar o valor gerado de numAleatorio como o matiz (hue)
-    // Saturação é 70% e Luminosidade é 80%, o que dá cores claras e vivas.
-    const corAleatoria = `hsl(${numAleatorio}, 70%, 80%)`;
-
-    // Aplica a cor aleatória gerada como cor de fundo da página
-    // document.body se refere ao elemento <body> da página HTML
-    // style.backgroundColor altera a cor de fundo do corpo da página
-    document.body.style.backgroundColor = corAleatoria;
-}
-
-// Adiciona um evento de "click" ao botão para chamar a função 'mudarACor'
-// Quando o botão for clicado, a função 'mudarACor' será executada, e a cor do fundo vai mudar
-mudarCorFundoBotao.addEventListener('click', mudarACor);
-
-/* ********************* */
-/* Formulário interativo */
-/* ********************* */
-
-// Seleciona o campo de input da pagina HTML onde o usuário vai digitar o nome
-// A variável nomeInputElement guarda o elemento com o ID 'nomeInput'
-const nomeInputElement = document.getElementById('nomeInput');
-
-// Seleciona o botão de saudação da página HTML
-// A variável saudarBotao guarda o elemento com o ID 'saudar'
-const saudarBotao = document.getElementById('saudar');
-
-// Seleciona o elemento saudacaoElento da página HTML
-// A variável saudacaoElento guarda o elemento com o ID 'saudacao'
-const saudacaoElento = document.getElementById('saudacao');
-
-// Define a função que será chamada quando o botão de saudação for clicado
-function criarSaudacao() {
-    // Pega o valor digitado no campo de input (nome) e remove espaços extras antes e depois
-    // O método 'trim()' elimina qualquer espaço em branco do começo e do fim da string
-    const nome = nomeInput.value.trim();
-
-    // Capitaliza a primeira letra do nome e converte o restante para minúsculas
-    // 'charAt(0)' pega o primeiro caractere do nome e 'toUpperCase()' transforma ele em maiúsculo
-    // 'slice(1)' pega o restante da string a partir do segundo caractere e 'toLowerCase()' transforma em minúsculas
-    const nomeCapitalizado = nome.charAt(0).toUpperCase() + nome.slice(1).toLowerCase();
-
-    // Verifica se o nome foi digitado (não está vazio)
-    if (nomeCapitalizado) {
-        // Se o nome não estiver vazio, exibe a saudação personalizada
-        // A template string (com crase) permite inserir o nome dentro da mensagem
-        saudacaoElento.textContent = `Olá, ${nomeCapitalizado}! Boas Vindas à prática de JavaScript!`;
-    } else {
-        // Se o nome estiver vazio (não foi preenchido), exibe uma mensagem padrão
-        saudacaoElento.textContent = `Olá, tem alguém ai?`;
-    }
-}
-
-// Adiciona um "listener" de evento no botão de saudação
-// 'addEventListener' vai escutar o clique do usuário no botão e chamar a função 'criarSaudacao' quando isso acontecer
-saudarBotao.addEventListener('click', criarSaudacao);
-
-/* ****************** */
-/* Galeria de Imagens */
-/* ****************** */
-
-// Seleciona o botão de "adicionar imagem" da página HTML
-// A variável adicionarImagemBotao guarda o elemento com o ID 'adicionarImagem'
-const adicionarImagemBotao = document.getElementById('adicionarImagem');
-
-// Seleciona o elemento de galeria onde as imagens serão adicionadas
-// O método 'getElementById' também é utilizado para pegar o elemento com o ID 'galeria'
-const galeriaElemento = document.getElementById('galeria');
-
-// Define a Função que será chamada quando o botão de adicionar imagem for clicado
-function criarGaleria() {
-    // Cria um novo elemento de imagem (<img>) na página
-    // O método 'createElement' cria um novo elemento HTML especificado como argumento
-    const tagImg = document.createElement('img');
-
-    // Define o atributo 'src' da imagem com um URL de uma imagem aleatória
-    // A API 'https://picsum.photos' foi usada para gerar uma imagem aleatóriait 
-    // 'Math.random()' gera um número aleatório, que é anexado à URL para garantir uma nova imagem a cada clique
-    tagImg.src = `https://picsum.photos/100?random=${Math.random()}`;
-
-    // Adiciona a imagem criada ao final do elemento de galeria
-    // 'appendChild' é usado para adicionar o novo elemento de imagem dentro do elemento 'galeria'
-    galeriaElemento.appendChild(tagImg);
-}
-
-// Adiciona um evento de clique no botão "adicionarImagemBotao"
-// Quando o botão for clicado, a função 'criarGaleria' será executada e uma nova imagem será adicionada à galeria
-adicionarImagemBotao.addEventListener('click', criarGaleria);
-
-/* ********* */
-/* conômetro */
-/* ********* */
-
-const contadorConometroElemento = document.getElementById('contadorConometro');
-const iniciarConometroBotao = document.getElementById('iniciarConometro');
-const pausarConometroBotao = document.getElementById('pausarConometro');
-const pararConometroBotao = document.getElementById('pararConometro');
-const zerarConometroBotao = document.getElementById('zerarConometro');
-const voltaConometroBotao = document.getElementById('voltaConometro');
-const mostrarVoltasElmento = document.getElementById('mostrarVoltas');
-
-let numeroContagem = 0;
-let sePausado = null;
-let seLigado = false;
-let guardarVoltas = [];
-let indiceDasVoltas = 0;
-let calculaVolta = 0;
-let tempoFormatado = 0;
-
-function pressionarBotao(botoes) {
-    switch (botoes) {
-        case 'iniciar':
-            if (!sePausado && !seLigado) {
-                resetarCronometro('zerar');
-                iniciarContagem();
-                sePausado = false;
-                seLigado = true;
-            }
-            break;
-        case 'pausar':
-            if (!sePausado && seLigado) {
-                clearTimeout(contador);
-                sePausado = true;
-                pausarConometroBotao.classList.add('retomar');
-                pausarConometroBotao.textContent = "Retomar";
-            } else if (sePausado && seLigado) {
-                sePausado = false;
-                iniciarContagem();
-                pausarConometroBotao.classList.remove('retomar');
-                pausarConometroBotao.textContent = "Pausar";
-            }
-            break;
-        case 'parar':
-            clearTimeout(contador);
-            resetarCronometro();
-            numeroContagem = 0;
-            seLigado = false;
-            sePausado = null;
-            pausarConometroBotao.classList.remove('retomar');
-            pausarConometroBotao.textContent = "Pausar";
-            break;
-        case 'zerar':
-            clearTimeout(contador);
-            resetarCronometro('zerar');
-            numeroContagem = 0;
-            seLigado = false;
-            sePausado = null;
-            pausarConometroBotao.classList.remove('retomar');
-            pausarConometroBotao.textContent = "Pausar";
-            contadorConometroElemento.innerText = formatarTempo(numeroContagem);
-            break;
-        case 'registrar':
-            criarRegistroDasVoltas(numeroContagem);
-            break;
-        default:
-            break;
-    }
-}
-
-function criarRegistroDasVoltas(RegistroDasVoltas) {
-
-
-    if (!sePausado && seLigado) {
-        guardarVoltas.push(RegistroDasVoltas);
-        if (indiceDasVoltas >= 1) {
-            
-        calculaVolta = (guardarVoltas[indiceDasVoltas]) - (guardarVoltas[indiceDasVoltas-1]);
-        indiceDasVoltas++;
-        tempoFormatado = formatarTempo(calculaVolta);
-        } else {
-            
-        tempoFormatado = formatarTempo(guardarVoltas[indiceDasVoltas]);
-        indiceDasVoltas++;
+  // Função para desenhar o mago em ação no canvas
+  desenhar: function (acao, posX, posY) {
+    let acaoEscolhida = this[acao];  // Pega a matriz da ação escolhida
+    if (acaoEscolhida) {
+      for (let i = 0; i < acaoEscolhida.matriz.length; i++) {
+        for (let j = 0; j < acaoEscolhida.matriz[i].length; j++) {
+          if (acaoEscolhida.matriz[i][j] === 1) {  // Verifica se é parte da "imagem"
+            ctx.fillStyle = "white";  // Pode mudar a cor ou o estilo conforme necessário
+            ctx.fillRect((posX * 10) + j * tamanhoPixel, (posY * 10) + i * tamanhoPixel, tamanhoPixel, tamanhoPixel);
+          }
         }
-        const tagP = document.createElement('p');
-        tagP.id = `volta-${indiceDasVoltas}`; 
-        tagP.textContent = `Volta ${indiceDasVoltas}: ${tempoFormatado}`; 
-        mostrarVoltas.appendChild(tagP);
+      }
+    }
+  }
+};
+
+// Função para desenhar o mago em qualquer posição com um único objeto
+function desenharMago(acao, posicao) {
+  let { x, y } = posicao; // Desestruturando o objeto para obter x e y
+  mago.desenhar(acao, x, y); // Usando a função de desenhar com a nova posição
+}
+
+// chamando a função desenharMago
+desenharMago("parado", { x: 1, y: 1 });
+desenharMago("andando1", { x: 1, y: 19 });
+desenharMago("andando2", { x: 16, y: 19 });
+
+let posX = -5; // Posição inicial do mago no eixo X
+let posY = 1; // Posição inicial no eixo Y
+let velocidade = 2.5; // Quantidade de pixels que ele se move por frame
+let passo = 1; // Alterna entre os sprites
+
+function loop() {
+    ctx.clearRect(0, 0, canvasLoading.width, canvasLoading.height); // Limpa o canvas
+
+    // Desenha o mago na nova posição
+    if (passo === 1) {
+        desenharMago("andando1", { x: posX, y: posY });
+        passo = 2;
+    } else if (passo === 2) {
+        desenharMago("parado", { x: posX, y: posY });
+        passo = 3;
+    } else {
+        desenharMago("andando2", { x: posX, y: posY });
+        passo = 1;
+    }
+
+    // Move o mago para a direita
+    posX += velocidade;
+
+    if (posX * tamanhoPixel > 80) {
+        posX = -5; // Volta para o começo
+    }
+
+    setTimeout(() => {
+        requestAnimationFrame(loop);
+    }, 200);  // Altera o sprite a cada 200ms
+}
+
+(function() {
+    var loader = document.getElementById("loader");
+    if (loader) {
+      loader.style.display = "flex";
+    }
+
+    loop();
+  
+    var dots = document.getElementById("dots");
+  
+    function updateDots() {
+      if (dots.textContent.length >= 3) {
+        dots.textContent = "";
+      } else {
+        dots.textContent += ".";
+      }
+    }
+    
+    var intervalId = setInterval(updateDots, 333);
+  
+    // Registra o tempo de início
+    var startTime = new Date().getTime();
+    var minDuration = 3000; // 3 segundos
+  
+    window.addEventListener("load", function() {
+      var elapsed = new Date().getTime() - startTime;
+      var remaining = minDuration - elapsed;
+      if (remaining < 0) {
+        remaining = 0;
+      }
+      
+      setTimeout(function() {
+        clearInterval(intervalId);
+        if (loader) {
+          loader.style.display = "none";
+        }
+      }, remaining);
+    });
+  })();
+  
+/*************************
+ * ATUALIZAÇÃO CADASTRAL *
+ *************************/
+// Seleciona o elemento onde o ano será exibido
+const anoAtualElement = document.getElementById("ano-atual");
+
+// Obtém o ano atual
+const anoAtual = new Date().getFullYear();
+
+// Atualiza o conteúdo do elemento com o ano atual
+anoAtualElement.innerHTML = `<a href="https://bmfolio.web.app/" target="_blank" rel="noopener noreferrer">
+    © ${anoAtual} Bruno Moraes - Evoluindo a cada código
+</a> | Licença AGPL v3`;
+
+/* ****************************
+ * AJUSTE DINÂMICO DE FONTES  *
+ **************************** */
+
+function obterDimensoesDaTela() {
+    return {
+        largura: window.innerWidth,
+        altura: window.innerHeight
     }
 }
-function resetarCronometro(botoes) {
 
+const ALTURA_REFERENCIA = 914;
+const FONTE_REFERENCIA = 16;
 
-    if (botoes === 'zerar') {
-        mostrarVoltas.innerHTML = '';  // Limpa as tags <p> quando zerar é pressionado
-        console.log('entrou no if de zerar');
-    }
-    guardarVoltas = [];  // Ou guardarVoltas.length = 0;
-    indiceDasVoltas = 0;
-    tempoAtual = 0;  // Exemplo, se houver um contador de tempo.
-    console.log("Cronômetro resetado e pronto para novas voltas.");
+function atualizarTamanhoDaFonte(alturaTela) {
+    let tamanhoDaFonte = Math.floor((FONTE_REFERENCIA * alturaTela) / ALTURA_REFERENCIA);
+    document.documentElement.style.setProperty('--tamanho-da-fonte', tamanhoDaFonte + 'px');
 }
 
-function formatarTempo(formatarSegundos) {
-    const horas = Math.floor(formatarSegundos / 3600);
-    const minutos = Math.floor((formatarSegundos % 3600) / 60);
-    const segundos = Math.floor(formatarSegundos % 60);
-
-    return `${String(horas).padStart(2, '0')}:${String(minutos).padStart(2, '0')}:${String(segundos).padStart(2, '0')}`;
+function aoRedimensionar() {
+    let dimensoes = obterDimensoesDaTela();
+    atualizarTamanhoDaFonte(dimensoes.altura);
 }
 
-function iniciarContagem() {
-    numeroContagem++;
-    /*contadorConometroElemento.innerText = numeroContagem;*/
-    contadorConometroElemento.innerHTML = formatarTempo(numeroContagem)
-
-    contador = setTimeout(iniciarContagem, 1000);
+function debounce(funcao, tempo) {
+    let tempoEspera;
+    return function () {
+        clearTimeout(tempoEspera);
+        tempoEspera = setTimeout(funcao, tempo);
+    };
 }
 
+window.addEventListener('resize', debounce(aoRedimensionar, 100));
 
-function iniciarContador() {
-    pressionarBotao('iniciar');
-}
-function pausarContador() {
-    pressionarBotao('pausar');
-}
-function pararContador() {
-    pressionarBotao('parar');
-}
-function zerarContador() {
-    pressionarBotao('zerar');
-}
-function registrarVolta() {
-    pressionarBotao('registrar');
-}
-
-iniciarConometroBotao.addEventListener('click', iniciarContador);
-pausarConometroBotao.addEventListener('click', pausarContador);
-pararConometroBotao.addEventListener('click', pararContador);
-zerarConometroBotao.addEventListener('click', zerarContador);
-voltaConometroBotao.addEventListener('click', registrarVolta);
+(function inicializar() {
+    aoRedimensionar();
+})();
